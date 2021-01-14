65dbcfc97496e92cef5009a796a577ff09907889
Jaiden <jaiden_chicote@hotmail.com>
2021-01-15 01:08:06 +1100

    [CS01] Changing input to Enhanced Touch

--- Assets/Scripts/Player/MobileInputManager.cs
+++ Assets/Scripts/Player/MobileInputManager.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.LowLevel;
+//using UnityEngine.InputSystem.LowLevel;
+using UnityEngine.InputSystem.EnhancedTouch;
+using UnityEngine.InputSystem.Controls;
 
 public interface IMobileInput
 {
@@ -11,8 +13,16 @@ public interface IMobileInput
 
 public class MobileInputManager : MonoBehaviour, IMobileInput
 {
+    /*public delegate void StartTouchEvent(Vector2 position, float time);
+    public event StartTouchEvent OnStartTouch;
+    public delegate void EndTouchEvent(Vector2 position, float time);
+    public event EndTouchEvent OnEndTouch;*/
+
+
     private ITouchLeftNavpadControl leftNavpad;
-    private TouchState touch;
+    //private TouchState touch;
+    private TouchControl touchControl;
+
 
     private Vector2 lastTouchPosition;
 
@@ -22,7 +32,25 @@ public class MobileInputManager : MonoBehaviour, IMobileInput
     public void InitialiseInput(GameObject mobileHUD)
     {
         leftNavpad = mobileHUD.GetComponent<ITouchLeftNavpadControl>();
+        touchControl = new TouchControl();
     }
+
+    protected void OnEnable()
+    {
+        EnhancedTouchSupport.Enable();
+        TouchSimulation.Enable();
+
+        //UnityEngine.InputSystem.EnhancedTouch.Touch.onFingerDown += FingerDown;
+    }
+
+    protected void OnDisable()
+    {
+        EnhancedTouchSupport.Disable();
+        TouchSimulation.Disable();
+
+        //UnityEngine.InputSystem.EnhancedTouch.Touch.onFingerDown -= FingerDown;
+    }
+
 
     /// <summary>
     /// 
@@ -33,11 +61,12 @@ public class MobileInputManager : MonoBehaviour, IMobileInput
         //Debug.Log(touch.phase);
         //Debug.Log("Hold position at: " + touch.position);
         //Debug.Log(value.Get<TouchState>());
-        touch = value.Get<TouchState>();
-        Debug.Log(touch.ToString());
+        //touch = value.Get<TouchState>();
+        //Debug.Log(touch.ToString());
+
 
         //Determine whether movement topuch detection is on the correct screen side
-        if (touch.position.x <= Screen.width / 2)
+        /*if (touch.position.x <= Screen.width / 2)
         {
 
             if (touch.phase == UnityEngine.InputSystem.TouchPhase.Began)
@@ -54,6 +83,26 @@ public class MobileInputManager : MonoBehaviour, IMobileInput
             {
                 leftNavpad.HidePad();
             }
+        }*/
+    }
+
+    /*private void FingerDown(Finger finger)
+    {
+        if (OnStartTouch != null) OnEndTouch(finger.screenPosition, Time.time);
+    }
+
+    private void FingerUp(Finger finger)
+    {
+        if (OnStartTouch != null) OnStartTouch(finger.screenPosition, Time.time);
+    }*/
+
+    private void Update()
+    {
+        var activeTouches = UnityEngine.InputSystem.EnhancedTouch.Touch.activeTouches;
+        for (var i = 0; i < activeTouches.Count; i++)
+        {
+            Debug.Log("Active touch: " + activeTouches[i]);
+            Debug.Log("Position at: " + activeTouches[i].screenPosition);
         }
     }
 
