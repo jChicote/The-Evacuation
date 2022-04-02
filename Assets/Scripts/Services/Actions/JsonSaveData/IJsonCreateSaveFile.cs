3198ff7e689dab843902946fe9306f50fb9b2751
Jaiden Chicote <jaiden_chicote@outlook.com>
2022-04-02 22:51:24 +1100

    feat: add file saving interface
    EVAC-15

---
+++ Assets/Scripts/Services/Actions/JsonSaveData/IJsonCreateSaveFile.cs
@@ -0,0 +1,15 @@
+namespace TheEvacuation.Services.Actions.JsonSaveData
+{
+
+    public interface IJsonCreateSaveFile
+    {
+
+        #region - - - - - - Methods - - - - - -
+
+        void CreateSaveFile(string fileName);
+
+        #endregion Methods
+
+    }
+
+}
