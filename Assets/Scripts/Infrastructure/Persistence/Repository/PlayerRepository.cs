2d3041075821b9e64853c867711859c1570bb384
Jaiden Chicote <jaiden_chicote@outlook.com>
2022-04-02 22:47:43 +1100

    fix: Get data context interface on Awake
    EVAC-15

--- Assets/Scripts/Infrastructure/Persistence/Repository/PlayerRepository.cs
+++ Assets/Scripts/Infrastructure/Persistence/Repository/PlayerRepository.cs
@@ -5,6 +5,14 @@ namespace TheEvacuation.Infrastructure.Persistence.Repository
 
     public class PlayerRepository : Repository<Player>
     {
+
+        #region - - - - - - Methods - - - - - -
+
+        private void Awake()
+            => context = this.GetComponent<IDataContext>();
+
+        #endregion Methods
+
     }
 
 }
