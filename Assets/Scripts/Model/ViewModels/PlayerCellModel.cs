84c4a391b5bdb116db5c8d85c7ef407a57a19ccb
Jaiden Chicote <jaiden_chicote@outlook.com>
2022-04-17 00:16:05 +1000

    feat: Creating view model objects for MVC
    EVAC-25

---
+++ Assets/Scripts/Model/ViewModels/PlayerCellModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace TheEvacuation.Model.ViewModels
+{
+    public class PlayerCellModel
+    {
+
+        #region - - - - - - Fields - - - - - -
+
+        public Guid id;
+        public Sprite avatar;
+        public string name;
+        public string score;
+
+        #endregion Fields
+
+    }
+
+}
