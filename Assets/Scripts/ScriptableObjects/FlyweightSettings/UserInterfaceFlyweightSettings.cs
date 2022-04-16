c8c585b2c8add6219e5730154b89359dd8d02866
Jaiden Chicote <jaiden_chicote@outlook.com>
2022-04-17 00:16:24 +1000

    feat: Created flywight UI settings
    EVAC-25

---
+++ Assets/Scripts/ScriptableObjects/FlyweightSettings/UserInterfaceFlyweightSettings.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace TheEvacuation.ScriptableObjects.FlyweightSettings
+{
+
+    [CreateAssetMenu(menuName = "Settings/User Interface Flyweight Settings")]
+    public class UserInterfaceFlyweightSettings : ScriptableObject
+    {
+
+        #region - - - - - - Fields - - - - - -
+
+        #endregion Fields
+
+    }
+
+}
