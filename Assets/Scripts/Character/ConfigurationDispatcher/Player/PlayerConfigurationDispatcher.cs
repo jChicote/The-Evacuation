6f124ac4110bbd64c3c6143115fa2f9cbdc3d62d
Jaiden Chicote <jaiden_chicote@outlook.com>
2022-05-14 21:31:32 +1000

    feat: Updated dispatcher to include health systems
    EVAC-12

--- Assets/Scripts/Character/ConfigurationDispatcher/Player/PlayerConfigurationDispatcher.cs
+++ Assets/Scripts/Character/ConfigurationDispatcher/Player/PlayerConfigurationDispatcher.cs
@@ -1,6 +1,8 @@
+using TheEvacuation.Interfaces.GameInterfaces.VitalityBars;
 using TheEvacuation.Model.Entities;
 using TheEvacuation.PlayerSystems.Input;
 using TheEvacuation.PlayerSystems.Movement;
+using TheEvacuation.PlayerSystems.Vitality;
 using UnityEngine;
 
 namespace TheEvacuation.Character.ConfigurationDispatcher.Player
@@ -8,11 +10,20 @@ namespace TheEvacuation.Character.ConfigurationDispatcher.Player
 
     public class PlayerInputConfigurationPort
     {
+
+        #region - - - - - - Properties - - - - - -
+
         public SpaceShip SpaceShip { get; set; }
+
+        public IPlayerHealthBar HealthBar { get; set; }
+
+        #endregion Properties
+
     }
 
     public class PlayerConfigurationDispatcher : MonoBehaviour, IConfigurationDispatcher<PlayerInputConfigurationPort>
     {
+
         #region - - - - - - Methods - - - - - -
 
         public void ConfigureGameObjectSystems(PlayerInputConfigurationPort inputPort)
@@ -22,6 +33,9 @@ namespace TheEvacuation.Character.ConfigurationDispatcher.Player
 
             IShipMovementSystem shipMovementSystem = GetComponent<IShipMovementSystem>();
             shipMovementSystem.InitialiseShipMovementSystem(inputPort.SpaceShip.shipAttributes);
+
+            IPlayerHealthSystem healthSystem = this.GetComponent<IPlayerHealthSystem>();
+            healthSystem.InitialisePlayerHealthSystem(inputPort.HealthBar, inputPort.SpaceShip.shipAttributes);
 
             IInputToggling inputToggle = this.GetComponent<IInputToggling>();
             inputToggle.ToggleInputActivation(true);
