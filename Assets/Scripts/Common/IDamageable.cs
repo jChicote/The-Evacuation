01809a5d83d806551294a4355db59611a68a5854
Jaiden Chicote <jaiden_chicote@outlook.com>
2022-05-14 21:31:47 +1000

    feat: Add damageable class
    EVAC-12

---
+++ Assets/Scripts/Common/IDamageable.cs
@@ -0,0 +1,15 @@
+namespace TheEvacuation.Common
+{
+
+    public interface IDamageable
+    {
+
+        #region - - - - - - Methods - - - - - -
+
+        void OnDamage(int damage);
+
+        #endregion Methods
+
+    }
+
+}
