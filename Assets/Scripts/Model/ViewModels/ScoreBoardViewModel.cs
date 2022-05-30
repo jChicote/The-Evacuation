112428ba5b48ca080dce45e516f0a5495e8b61ee
Jaiden Chicote <jaiden_chicote@outlook.com>
2022-05-30 22:54:44 +1000

    feat: Create score board panel view model
    EVAC-14

---
+++ Assets/Scripts/Model/ViewModels/ScoreBoardViewModel.cs
@@ -0,0 +1,17 @@
+namespace TheEvacuation.Model.ViewModels
+{
+
+    public class ScoreboardViewModel
+    {
+
+        #region - - - - - - Properties - - - - - -
+
+        public int CurrentScore { get; set; }
+
+        public int TotalScore { get; set; }
+
+        #endregion Properties
+
+    }
+
+}
